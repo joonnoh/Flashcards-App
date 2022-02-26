3ee0a3ef05f561c6a679d3838930a311dca62cbc
joonnoh <71247743+joonnoh@users.noreply.github.com>
2022-02-26 15:02:44 -0500

    Create Flashcard project with initial code

--- Flashcards App/ViewController.swift
+++ Flashcards App/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapOnFlashcard(_ sender: Any) {
+        if questionLabel.isHidden == false {
+            questionLabel.isHidden = true
+        }
+        else {
+            questionLabel.isHidden = false
+        }
+        
+    }
+    
 }
 
